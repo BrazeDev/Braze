57e7bf1ce3a0df57856f9bb5556c403cdf0cb162
thatsimplekid <thatsimplekid@gmail.com>
2021-02-23 22:53:10 +0000

    Fixed .gitignore (untrack ignored files)

--- .nuxt/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as HeaderBar } from '../../components/HeaderBar.vue'
-export { default as SideBar } from '../../components/SideBar.vue'
-
-export const LazyHeaderBar = import('../../components/HeaderBar.vue' /* webpackChunkName: "components/header-bar" */).then(c => c.default || c)
-export const LazySideBar = import('../../components/SideBar.vue' /* webpackChunkName: "components/side-bar" */).then(c => c.default || c)
